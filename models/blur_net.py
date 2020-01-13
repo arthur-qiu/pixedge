7d203f22dac4438583af4dfc7279527dfe6f206c
邱浩楠 <Arthur>
2020-01-13 19:26:40 +0800

    a

--- models/blur_net.py
+++ models/blur_net.py
@@ -14,9 +14,10 @@ class BlurNet(nn.Module):
                                       [0.02324684, 0.03382395, 0.03832756, 0.03382395, 0.02324684]])
 
         self.filter_weight1 = filter_weight.view(1, 1, 5, 5).repeat(3, 1, 1, 1).cuda()
+        self.pad = nn.ReflectionPad2d((2, 2, 2, 2))
 
     def forward(self, x):
-        out = nn.functional.conv2d(x, self.filter_weight1, groups = 3)
+        out = nn.functional.conv2d(self.pad(x), self.filter_weight1, groups = 3)
         out = nn.functional.interpolate(out, scale_factor=0.5, mode = 'bicubic')
         out = nn.functional.interpolate(out, scale_factor=2, mode = 'nearest')
         return out
